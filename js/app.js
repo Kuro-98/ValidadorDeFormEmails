e9b5679a57383e2bfad1d1ed6e567e07764cfc92
Kuro-98 <101947718+Kuro-98@users.noreply.github.com>
2024-04-21 21:09:47 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+	//seleccionar los elementos de la interfaz
+	const inputEmail = document.querySelector('#email');
+	const inputAsunto = document.querySelector('#asunto');
+
+	console.log(inputAsunto);
+});
