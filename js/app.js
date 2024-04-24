4abc08c79534a75b75b94d04ff262be4c116d392
Kuro-98 <101947718+Kuro-98@users.noreply.github.com>
2024-04-23 20:33:57 -0600

    anadimos eventListeners

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
 	//seleccionar los elementos de la interfaz
 	const inputEmail = document.querySelector('#email');
 	const inputAsunto = document.querySelector('#asunto');
+	const inputMensaje = document.querySelector('#mensaje');
 
-	console.log(inputAsunto);
+	//asignar eventos
+	inputEmail.addEventListener('blur', function (e) {
+		console.log(e.target.value);
+	});
 });
