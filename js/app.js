99395aeff56e508e4f72f44df8ccbeec5808e80b
Emanuel Morales <101947718+Kuro-98@users.noreply.github.com>
2024-04-25 13:20:51 -0600

    fin de las validaciones del formulario

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
+	const email = {
+		email: '',
+		asunto: '',
+		mensaje: '',
+	};
+
 	//seleccionar los elementos de la interfaz
 	const inputEmail = document.querySelector('#email');
 	const inputAsunto = document.querySelector('#asunto');
 	const inputMensaje = document.querySelector('#mensaje');
 	const formulario = document.querySelector('#formulario');
+	const btnSumit = document.querySelector('#formulario button[type="submit"]');
+	const btnReset = document.querySelector('#formulario button[type="reset"]');
 
 	function limpiarAlerta(ref) {
 		const alerta = ref.querySelector('.bg-red-600');
@@ -28,20 +36,48 @@ document.addEventListener('DOMContentLoaded', () => {
 		referencia.appendChild(error);
 	}
 
+	function comprobarEmail() {
+		if (!Object.values(email).includes('')) {
+			btnSumit.classList.remove('opacity-50');
+			btnSumit.disabled = false;
+			return;
+		}
+		btnSumit.classList.add('opacity-50');
+		btnSumit.disabled = true;
+	}
+
 	function validar(e) {
 		if (e.target.value.trim() === '') {
 			mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
+			email[e.target.id] = '';
+			comprobarEmail();
 			return;
 		}
 
 		if (e.target.id === 'email' && !validarEmail(e.target.value)) {
 			mostrarAlerta('El email, no es valido', e.target.parentElement);
+			email[e.target.id] = '';
+			comprobarEmail();
 			return;
 		}
 		limpiarAlerta(e.target.parentElement);
+
+		//asignar los valores
+		email[e.target.id] = e.target.value.trim().toLowerCase();
+
+		comprobarEmail();
 	}
 
 	inputEmail.addEventListener('blur', validar);
 	inputAsunto.addEventListener('blur', validar);
 	inputMensaje.addEventListener('blur', validar);
+	btnReset.addEventListener('click', function (e) {
+		e.preventDefault();
+		email.email = '';
+		email.asunto = '';
+		email.mensaje = '';
+
+		formulario.reset();
+		comprobarEmail();
+	});
 });
